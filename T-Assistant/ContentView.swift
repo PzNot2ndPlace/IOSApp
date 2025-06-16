ef2973e114fc84eedcfad20546d2718cc4ddf935
BogdanTarchenko <tarchenko.bogdan@outlook.com>
2025-06-16 13:36:49 +0700

    Initial Commit

---
+++ T-Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T-Assistant
+//
+//  Created by Богдан Тарченко on 16.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ T-Assistant/T_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_AssistantApp.swift
+//  T-Assistant
+//
+//  Created by Богдан Тарченко on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct T_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
